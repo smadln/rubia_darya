48785adb8ab18a11fb3ace746ba316b50a965623
Sarah Madeline <e.sarah.madeline@gmail.com>
2024-04-29 18:16:36 -0700

    Integrate auto-rotation feature

--- src/script.js
+++ src/script.js
@@ -12,20 +12,16 @@ let lightMode = true
 const clock = new THREE.Clock()
 
 // Set rotate boolean variable
-let rotateModel = false
-
-//Ugh, don't ask about this stuff
-var userUploaded = false
-let controls
+let rotateModel = false  // Initially set to false, toggle via UI
 
 // Creates empty mesh container
 const myMesh = new THREE.Mesh();
 
-// Scene
+// Scene setup
 const scene = new THREE.Scene()
 scene.background = new THREE.Color(0, 0, 0);
 
-//Lights
+// Lights
 const pointLight1 = new THREE.PointLight(0xffffff, 1);
 pointLight1.position.set(100, 100, 400);
 scene.add(pointLight1);
@@ -34,28 +30,25 @@ const pointLight2 = new THREE.PointLight(0xffffff, .5);
 pointLight2.position.set(-500, 100, -400);
 scene.add(pointLight2);
 
-// Parameters
+// STL Loader for model import
 const stlLoader = new STLLoader()
 
-//Material
+// Material configuration
 const material = new THREE.MeshStandardMaterial()
 material.flatShading = true
 material.side = THREE.DoubleSide;
 
-// Sizes
+// Renderer setup
+const renderer = new THREE.WebGLRenderer()
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-// Camera
+// Camera setup
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 2000)
 
-// Renderer
-const renderer = new THREE.WebGLRenderer()
-
 let effect;
-
 let characters = ' .:-+*=#'
 const effectSize = { amount: .205 }
 let backgroundColor = 'lightblue'
@@ -69,108 +62,48 @@ function createEffect() {
 }
 
 createEffect()
-
 document.body.appendChild(effect.domElement)
-
 document.getElementById("ascii").style.whiteSpace = "prewrap"
 
+// Load and display the STL model
 stlLoader.load(
     '3dpea copy.stl',
     function (geometry) {
-
         myMesh.material = material;
         myMesh.geometry = geometry;
-
-        var tempGeometry = new THREE.Mesh(geometry, material)
-        myMesh.position.copy = (tempGeometry.position)
-
         geometry.computeVertexNormals();
-        myMesh.geometry.center()
-
-        myMesh.rotation.x = -90 * Math.PI / 180;
-
-        myMesh.geometry.computeBoundingBox();
-        var bbox = myMesh.geometry.boundingBox;
-
-        myMesh.position.y = ((bbox.max.z - bbox.min.z) / 5)
-
-        camera.position.x = ((bbox.max.x * 4));
-        camera.position.y = ((bbox.max.y));
-        camera.position.z = ((bbox.max.z * 3));
+        myMesh.geometry.center();
+        myMesh.rotation.x = -Math.PI / 2;
 
         scene.add(myMesh);
-
-
-        controls = new OrbitControls(camera, effect.domElement)
-
-
-        function tick() {
-            if (rotateModel) {
-                const elapsedTime = clock.getElapsedTime();
-                myMesh.rotation.z = elapsedTime / 3;
-            }
-        
-            render();
-            window.requestAnimationFrame(tick);
-        }
-        
-        function render() {
-            effect.render(scene, camera);
-        }
-        
-        tick(); // Starts the rendering loop
-        
-
-        document.getElementById('file-selector').addEventListener('change', openFile, false);
-
-
-        function openFile(evt) {
-            const fileObject = evt.target.files[0];
-
-            const reader = new FileReader();
-            reader.readAsArrayBuffer(fileObject);
-            reader.onload = function () {
-                if (userUploaded == false) {
-                    userUploaded = true;
-                }
-                const geometry = stlLoader.parse(this.result);
-                tempGeometry = geometry;
-                myMesh.geometry = geometry;
-                myMesh.geometry.center()
-
-                myMesh.rotation.x = -90 * Math.PI / 180;
-
-                myMesh.geometry.computeBoundingBox();
-                var bbox = myMesh.geometry.boundingBox;
-
-                // camera.position.x = ((bbox.max.x * 4));
-                // camera.position.y = ((bbox.max.y));
-                // camera.position.z = ((bbox.max.z * 3));
-
-                myMesh.position.y = ((bbox.max.z - bbox.min.z) / 6)
-
-                scene.add(myMesh);
-            };
-        };
+        controls = new OrbitControls(camera, effect.domElement);
+        animate(); // Starts the rendering loop
     }
-)
+);
 
-
-document.getElementById('screenshotButton').addEventListener('click', takeScreenshot);
-
-function takeScreenshot() {
-    var container = document.body; // full page 
-    html2canvas(container).then(function (canvas) {
-
-        var link = document.createElement("a");
-        document.body.appendChild(link);
-        link.download = "ASCII.jpg";
-        link.href = canvas.toDataURL("image/jpg");
-        console.log(link.href);
-        // link.target = '_blank';
-        link.click();
-    });
+// Rendering loop with auto-rotation
+function animate() {
+    requestAnimationFrame(animate);
+    if (rotateModel) {
+        const elapsedTime = clock.getElapsedTime();
+        myMesh.rotation.z = elapsedTime / 3;
+    }
+    effect.render(scene, camera);
 }
+
+// Event listeners for mouse and touch interaction
+document.addEventListener('mousedown', () => {
+    rotateModel = false;
+});
+document.addEventListener('mouseup', () => {
+    rotateModel = true;
+});
+document.addEventListener('touchstart', () => {
+    rotateModel = false;
+});
+document.addEventListener('touchend', () => {
+    rotateModel = true;
+});
 
 document.getElementById('rotateButton').addEventListener('click', toggleRotation);
 
@@ -179,105 +112,11 @@ function toggleRotation() {
     console.log('Rotate mode toggled. Current value: ', rotateModel);
 }
 
-document.getElementById('updateASCII').addEventListener('click', updateASCII);
-
-function updateASCII() {
-
-    document.body.removeChild(effect.domElement)
-
-    characters = " " + "." + document.getElementById('newASCII').value;
-
-    createEffect()
-    onWindowResize()
-
-    document.body.appendChild(effect.domElement)
-
-    controls = new OrbitControls(camera, effect.domElement)
-
-}
-
-document.getElementById('resetASCII').addEventListener('click', resetASCII);
-
-function resetASCII() {
-
-    document.body.removeChild(effect.domElement)
-
-    characters = ' .:-+*=%@#'
-
-    createEffect()
-    onWindowResize()
-
-    document.body.appendChild(effect.domElement)
-
-    controls = new OrbitControls(camera, effect.domElement)
-}
-
-document.getElementById('lightDark').addEventListener('click', lightDark);
-
-function lightDark() {
-    lightMode = !lightMode
-    if (lightMode === true) {
-        document.getElementById("kofi").style.color = "lightblue";
-        document.body.style.backgroundColor = 'white';
-
-        backgroundColor = 'lightblue'
-        ASCIIColor = 'white'
-
-        effect.domElement.style.color = ASCIIColor;
-        effect.domElement.style.backgroundColor = backgroundColor;
-    } else {
-        document.getElementById("kofi").style.color = "white";
-        document.body.style.backgroundColor = 'lightblue';
-
-        backgroundColor = 'lightblue'
-        ASCIIColor = 'white'
-
-        effect.domElement.style.color = ASCIIColor;
-        effect.domElement.style.backgroundColor = backgroundColor;
-    }
-}
-
 window.addEventListener('resize', onWindowResize);
 
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-
     renderer.setSize(window.innerWidth, window.innerHeight);
     effect.setSize(window.innerWidth, window.innerHeight);
-}
-
-function download(filename, text) {
-    var element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    element.setAttribute('download', filename);
-
-    element.style.display = 'none';
-    document.body.appendChild(element);
-
-    element.click();
-
-    document.body.removeChild(element);
-}
-
-document.getElementById("copyASCII").addEventListener("click", function () {
-    var text = document.getElementsByTagName("table")[0].innerText
-    var filename = "ASCII.txt";
-
-    download(filename, text);
-}, false);
-
-document.getElementById("clipboardASCII").addEventListener("click", function () {
-    const textArea = document.createElement("textarea");
-    textArea.textContent = document.getElementsByTagName("td")[0].innerText;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
-    window.alert("ASCII copied to clipboard");
-}, false);
-
-function rotateMode() {
-    rotateModel = !rotateModel;
-    console.log('Rotate mode toggled. Current value: ', rotateModel);
 }
