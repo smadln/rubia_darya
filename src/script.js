ac94c8437a408f0104e3b2b18f5ddeb5c3ef6943
Sarah Madeline <e.sarah.madeline@gmail.com>
2024-04-29 21:12:45 -0700

    Add semicolon to each import

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
 import { AsciiEffect } from 'three/examples/jsm/effects/AsciiEffect.js';
 import html2canvas from 'html2canvas';
 
