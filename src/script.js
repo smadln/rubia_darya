331093de3e28b0ff27d00dfd7848b5e87abeb337
Sarah Madeline <e.sarah.madeline@gmail.com>
2024-04-30 00:20:49 -0700

    Edit js to test 3d rendering

--- src/script.js
+++ src/script.js
@@ -1,15 +1,17 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+console.log('If this shows, the JS file is linked and running.');
+
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
-
-camera.position.z = 5;
 
 function animate() {
     requestAnimationFrame(animate);
@@ -19,3 +21,5 @@ function animate() {
 }
 
 animate();
+console.log('If this shows, the JS file is linked and running.');
+
