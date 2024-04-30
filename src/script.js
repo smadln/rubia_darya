908974eccb4887501fb1ccb289d473c4f15041cf
Sarah Madeline <e.sarah.madeline@gmail.com>
2024-04-29 22:17:14 -0700

    Edit {html2canvas}

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
 import { AsciiEffect } from 'three/examples/jsm/effects/AsciiEffect.js';
-import html2canvas from 'html2canvas';
+import {html2canvas} from 'https://cdn.jsdelivr.net/npm/html2canvas@1.3.2/dist/html2canvas.min.js';
 
 // LightMode
 let lightMode = true;
