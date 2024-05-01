7bcb195f83c5d870d83b012081485fcd03f17886
Sarah Madeline <e.sarah.madeline@gmail.com>
2024-04-30 17:03:54 -0700

    Edit path THREE from 'three' to'./three.module.min.js'

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 import './style.css'
-import * as THREE from 'three'
+import * as THREE from './three.module.min.js'
 import { OrbitControls } from './OrbitControls.js'
 import { STLLoader } from './STLLoader.js'
 import { AsciiEffect } from './AsciiEffect.js';
